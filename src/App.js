306233c7e4c0d4d18e55a15af0df1ad57e6266cc
Yevhwnii <mirandoos3223@gmail.com>
2020-08-22 20:35:19 +0300

    finishing main application component

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ const App = () => {
     if (e.key === "Enter") {
       const data = await fetchWeather(query);
       setWeather(data);
+      setQuery("");
     }
   };
 
@@ -26,6 +27,26 @@ const App = () => {
         }}
         onKeyPress={search}
       />
+      {weather.main && (
+        <div className="city">
+          <h2 className="city-name">
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+          <div className="city-temp">
+            {Math.round(weather.main.temp)}
+            <sup>&deg;C</sup>
+          </div>
+          <div className="info">
+            <img
+              className="city-icon"
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt={weather.weather[0].description}
+            />
+            <p>{weather.weather[0].description}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
