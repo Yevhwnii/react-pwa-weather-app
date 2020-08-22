90c3f3078822161c4b142129251da681888b773c
Yevhwnii <mirandoos3223@gmail.com>
2020-08-22 20:24:11 +0300

    initial app setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
-const App = (props) => {
-  return <h1>HEY</h1>;
+import { fetchWeather } from "./api/fetchWeather";
+import "./App.css";
+
+const App = () => {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchWeather(query);
+      setWeather(data);
+    }
+  };
+
+  return (
+    <div className="main-container">
+      <input
+        type="text"
+        className="search"
+        placeholder="Search..."
+        value={query}
+        onChange={(e) => {
+          setQuery(e.target.value);
+        }}
+        onKeyPress={search}
+      />
+    </div>
+  );
 };
 
 export default App;
